3311b5a7cd2b8847b36c79e0c7f22c522a9e8500
LIN I-MIN <iminlin@17.media>
2021-12-05 20:21:59 +0800

    Remove SettingRemoteMediator

--- app/src/main/java/app/newproj/lbrytv/data/repo/SettingRepository.kt
+++ app/src/main/java/app/newproj/lbrytv/data/repo/SettingRepository.kt
@@ -1,30 +1,21 @@
 package app.newproj.lbrytv.data.repo
 
-import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import app.newproj.lbrytv.R
-import app.newproj.lbrytv.data.MainDatabase
-import app.newproj.lbrytv.data.dto.SettingType
 import app.newproj.lbrytv.data.entity.Setting
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
+import javax.inject.Provider
 
 private const val PAGE_SIZE = 100
 
-@OptIn(ExperimentalPagingApi::class)
 class SettingRepository @Inject constructor(
-    private val db: MainDatabase,
-    private val settingRemoteMediator: SettingRemoteMediator,
+    private val settingPagingSourceProvider: Provider<SettingPagingSource>,
 ) {
 
     fun settings(): Flow<PagingData<Setting>> = Pager(
         config = PagingConfig(pageSize = PAGE_SIZE),
-        remoteMediator = settingRemoteMediator,
-        pagingSourceFactory = { db.settingDao().settings() }
+        pagingSourceFactory = { settingPagingSourceProvider.get() }
     ).flow
 }
