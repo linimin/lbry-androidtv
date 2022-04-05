51ba1062649dbfac4dd08438bf92f3e3f6ecea0c
LIN I-MIN <iminlin@17.media>
2022-04-05 22:50:22 +0800

    Bump the version to v1.0.0-alpha.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         minSdk = 26
         targetSdk = 31
         versionCode = 1
-        versionName = "1.0.0-alpha.3"
+        versionName = "1.0.0-alpha.4"
         javaCompileOptions {
             annotationProcessorOptions {
                 /*
