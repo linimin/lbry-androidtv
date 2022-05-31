75bed7e7bae0b1ddaa2b5356b82dbec62c25776e
LIN I-MIN <iminlin@17.media>
2022-05-31 16:20:25 +0800

    Bump the version code to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
         applicationId = "app.newproj.lbrytv"
         minSdk = libs.versions.buildOptions.minSdk.get().toInt()
         targetSdk = libs.versions.buildOptions.targetSdk.get().toInt()
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.1-beta.5"
         // https://developer.android.com/training/dependency-injection/hilt-testing#instrumented-tests
         testInstrumentationRunner = "app.newproj.lbrytv.HiltTestRunner"
